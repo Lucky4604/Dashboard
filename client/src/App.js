5fe383307d5737c097fe8f239fe7931035a4f97f
lucky govind rao <luckygovindrao@192.168.1.110>
2024-07-16 21:36:23 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./components/Dashboard/Main";
+
+
+const AppRouter = () => {
+  return (
+    <>
+    <Main/>
+    </>
+  );
+};
+
+export default AppRouter;
---
+++ client/src/components/Dashboard/Navbar.jsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "../ui/navbar-menu";
+import { cn } from ""
+
